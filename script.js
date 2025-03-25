b800d016c028bb928e3fc97d17c58197135a49f5
leinadd18 <dan.dias1808@gmail.com>
2025-03-24 21:55:29 -0300

    Subindo primeiro commit

---
+++ script.js
@@ -0,0 +1,50 @@
+ // Seleciona todos os links que possuem um href que começa com #
+ const links = document.querySelectorAll('a[href^="#"]');
+
+ // Adiciona evento de clique para cada link
+ links.forEach(link => {
+     link.addEventListener('click', (event) => {
+         event.preventDefault(); // Impede o comportamento padrão do link
+
+         // Seleciona a seção de destino
+         const targetId = link.getAttribute('href').substring(1); // Pega o valor do href sem o "#"
+         const targetSection = document.getElementById(targetId);
+
+         // Calcula o deslocamento da barra de navegação fixa
+         const offsetTop = targetSection.offsetTop - 60; // Ajuste de 60px (altura da barra de navegação fixa)
+
+         // Rola suavemente até a seção
+         window.scrollTo({
+             top: offsetTop,
+             behavior: 'smooth'
+         });
+     });
+ });
+
+ //Animação do scroll
+ const elemento = document.querySelectorAll('.hidden');
+ const element = document.querySelectorAll('.hidden2');
+
+ const olha = new IntersectionObserver((entries) => {
+    entries.forEach((entre) => {
+        if(entre.isIntersecting){
+            entre.target.classList.add('show')
+        }else{
+            entre.target.classList.remove('show')
+        }
+    })
+ })
+
+ elemento.forEach((secao) =>  olha.observe(secao));
+
+ const olha2 = new IntersectionObserver((entries) => {
+    entries.forEach((entre) => {
+        if(entre.isIntersecting){
+            entre.target.classList.add('show2')
+        }else{
+            entre.target.classList.remove('show2')
+        }
+    })
+ })
+
+ element.forEach((secao) =>  olha2.observe(secao));
